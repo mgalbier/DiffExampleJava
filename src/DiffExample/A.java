02e513f8ce16a828530579b2ac6d5d1895a2c936
Manuel Galbier <manuel.galbier@gmail.com>
2016-08-18 11:37:48 +0200

    initial commit

---
+++ src/DiffExample/A.java
@@ -0,0 +1,9 @@
+package DiffExample;
+
+public class A {
+	int add(int arg)
+	{
+		int fixed = 1;
+		return arg+fixed;
+	}
+}
---
+++ src/DiffExample/B.java
@@ -0,0 +1,15 @@
+package DiffExample;
+
+public class B {
+	float mult(float a, float b)
+	{
+		return a*b;
+	}
+	
+	float square(float x)
+	{
+		float result = x*x;
+		System.out.println("Square is" + result);
+		return result;
+	}
+}
